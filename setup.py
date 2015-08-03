866046fd566667ea4a7eb47341e93f92dac04488
Simon Busard <simon.busard@gmail.com>
2015-08-03 14:48:56 +0200

    Add LICENSE, MANIFEST and setup files for distribution.

---
+++ setup.py
@@ -0,0 +1,34 @@
+"""
+Setup module for wagoner.
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as readme:
+    setup(
+        name="wagoner",
+        version="1.0",
+        description="A random word generator.",
+        long_description=readme.read(),
+        url="https://github.com/sbusard/wagoner",
+        author="Simon Busard",
+        author_email="simon.busard@gmail.com",
+        license="MIT",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4"
+        ],
+        keywords="random word generation",
+        packages=find_packages(),
+        scripts=[
+            "wagoner/table.py",
+            "wagoner/tree.py",
+            "wagoner/word.py"
+        ]
+    )
